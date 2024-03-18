da87d31ddeadd086484904cdce905f89d0c4c77a
MOHIT VERMA <66687560+vermaji123089@users.noreply.github.com>
2024-03-18 12:59:12 +0530

    change url of login to redirect admin

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -17,7 +17,7 @@ const Login = () => {
         if (result.data.Status === "success") {
           const role = result.data.role;
           if (role === "admin") {
-            navigate("/admin");
+            navigate("/https://doc-y3r4.onrender.com/admin");
           } else if (role === "user") {
             navigate("/");
             console.log(result);

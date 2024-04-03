9acc1ec5ec316483fa9d55a2e1e244839f67f4b4
MOHIT VERMA <66687560+vermaji123089@users.noreply.github.com>
2024-04-03 10:15:53 +0530

    Proud to be one of the nation best

--- frontend/src/components/About.jsx
+++ frontend/src/components/About.jsx
@@ -11,7 +11,7 @@ const About = () => {
    
     <div className='flex justify-between gap-[50px] lg:gap-[130px] xl:gap-0 flex-col lg:flex-row ' >
 {/* about img */}
-<div className=' relative w-3/4 lg:w-1/2 xl:w-[770px] z-10 order-2 lg:order-1 ' >
+<div className=' relative w-3/4 lg:w-1/2 xl:w-[770px] z-0 order-2 lg:order-1 ' >
 <img src={aboutImg} alt="" />
 <div className='absolute z-20 bottom-4 w-[200px] md:w-[-30%] md:right-[-7%] lg:right-[22%] ' >
 <img src={aboutImgCard} alt="" />
